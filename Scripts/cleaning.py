aee771ab0b0a222bf1f5363ebc33e9ba3417c929
Prashant Tiwari <prashant.st8@gmail.com>
2023-09-02 18:03:18 +0530

    script format corrected

--- Scripts/cleaning.py
+++ Scripts/cleaning.py
@@ -1,34 +1,38 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("Data/metadata.csv")
+def CleanData():
+    data = pd.read_csv("Data/metadata.csv")
 
-finds_array = []
-for i, disease in enumerate(data['finding']):
-    finds_array.append(disease)
+    finds_array = []
+    for i, disease in enumerate(data['finding']):
+        finds_array.append(disease)
 
-splits = []
+    splits = []
 
-for disease in finds_array:
-    if '/' in disease:
-        splits.extend(disease.split('/'))
-    else:
-        splits.append(disease)
+    for disease in finds_array:
+        if '/' in disease:
+            splits.extend(disease.split('/'))
+        else:
+            splits.append(disease)
 
-unique_values = np.unique(splits)
+    unique_values = np.unique(splits)
 
-for column in unique_values:
-    data[column] = 0
+    for column in unique_values:
+        data[column] = 0
 
-for i in range(0,len(data)):
-    diseases = data.loc[i, 'finding']
-    diseases = diseases.split('/')
+    for i in range(0,len(data)):
+        diseases = data.loc[i, 'finding']
+        diseases = diseases.split('/')
 
-    for disease in diseases:
-        data.loc[i, disease] = 1
+        for disease in diseases:
+            data.loc[i, disease] = 1
 
-data.drop(['license', 'patientid', 'folder', 'url', 'other_notes', 'clinical_notes', 'doi', 'finding', 'Unnamed: 29', 'location', 'neutrophil_count', 'lymphocyte_count', 'extubated', 'temperature', 'pO2_saturation', 'leukocyte_count', 'date', 'needed_supplemental_O2'], axis=1, inplace=True)
+    data.drop(['license', 'patientid', 'folder', 'url', 'other_notes', 'clinical_notes', 'doi', 'finding', 'Unnamed: 29', 'location', 'neutrophil_count', 'lymphocyte_count', 'extubated', 'temperature', 'pO2_saturation', 'leukocyte_count', 'date', 'needed_supplemental_O2'], axis=1, inplace=True)
 
-data.drop(data[data['filename'].str.endswith(".gz")].index, inplace=True)
+    data.drop(data[data['filename'].str.endswith(".gz")].index, inplace=True)
 
-data.to_csv('Data/cleaned_metadata.csv')
+    data.to_csv('Data/cleaned_metadata.csv')
+
+if __name__ == '__main__':
+    CleanData()
