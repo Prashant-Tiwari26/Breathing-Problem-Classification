5bf589d608ddb1cd3f4d3f9d8af11be80eab0b65
Prashant Tiwari <prashant.st8@gmail.com>
2023-09-16 21:35:05 +0530

    processed data split

--- Scripts/preprocess.py
+++ Scripts/preprocess.py
@@ -10,6 +10,7 @@ Dependencies: pandas, numpy, scikit-learn (StandardScaler)
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 def PreprocessData():
     """
@@ -23,8 +24,12 @@ def PreprocessData():
             data[column].replace({'N':0, 'Y':1, 'M':0, 'F':1, 'X-ray':0, 'CT':1, 'Unclear':0}, inplace=True)
         elif data[column].dtype == 'O' and len(data[column].unique()) >= 4 and len(data[column].unique()) <= 10:
             data = pd.get_dummies(data, columns=['view'], prefix=['view'])
-
-    data = data[['offset', 'sex', 'age', 'RT_PCR_positive', 'survival', 'intubated',
+ 
+    scale = StandardScaler()
+    data['offset_standardized'] = scale.fit_transform(np.array(data['offset']).reshape(-1,1))
+    data.drop(['offset'], axis=1, inplace=True)
+    
+    data = data[['offset_standardized', 'sex', 'age', 'RT_PCR_positive', 'survival', 'intubated',
        'intubation_present', 'went_icu', 'in_icu', 'view_AP', 'view_AP Erect', 'view_AP Supine', 'view_Axial',
        'view_Coronal', 'view_L', 'view_PA', 'modality', 'filename',
        'Aspergillosis', 'Aspiration', 'Bacterial', 'COVID-19', 'Chlamydophila',
@@ -33,12 +38,18 @@ def PreprocessData():
        'Nocardia', 'Pneumocystis', 'Pneumonia', 'SARS', 'Staphylococcus',
        'Streptococcus', 'Tuberculosis', 'Unknown', 'Varicella', 'Viral',
        'todo']]
-    
-    scale = StandardScaler()
-    data['offset_standardized'] = scale.fit_transform(np.array(data['offset']).reshape(-1,1))
-    data.drop(['offset'], axis=1, inplace=True)
 
-    data.to_csv("Data/processed_metadata.csv")
+    train_set, test_set = train_test_split(data, test_size=0.2, shuffle=True, random_state=1)
+    train_set, val_set = train_test_split(train_set, test_size=0.125, shuffle=True, random_state=1)
+
+    train_set.reset_index(inplace=True, drop=True)
+    test_set.reset_index(inplace=True, drop=True)
+    val_set.reset_index(inplace=True, drop=True)
+    
+    data.to_csv("Data/Processed/processed_metadata.csv")
+    train_set.to_csv("Data/Processed/train_set.csv")
+    test_set.to_csv("Data/Processed/test_set.csv")
+    val_set.to_csv("Data/Processed/val_set.csv")
 
 if __name__ == '__main__':
     PreprocessData()
