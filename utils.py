db5ef70c63588fb1c639f6a28b38f990ddc9ab36
Prashant Tiwari <prashant.st8@gmail.com>
2023-09-16 21:34:35 +0530

    train loop v2 added

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
+from sklearn.metrics import accuracy_score
 from torchvision.transforms import Compose, ToTensor, CenterCrop, Normalize, Resize
 
 transform = Compose([
@@ -89,10 +90,11 @@ def TrainLoop(
     optimizer:torch.optim.Optimizer,
     criterion:torch.nn.Module,
     train_dataloader:torch.utils.data.DataLoader,
-    test_dataloader:torch.utils.data.DataLoader,
-    val_dataloader:torch.utils.data.DataLoader=None,
+    val_dataloader:torch.utils.data.DataLoader,
+    test_dataloader:torch.utils.data.DataLoader=None,
     num_epochs:int=20,
     early_stopping_rounds:int=5,
+    return_best_model:bool=True,
     device:str='cpu'
 ):
     """
@@ -137,6 +139,8 @@ def TrainLoop(
     total_val_loss = []
     total_test_loss = []
 
+    best_model_weights = model.state_dict()
+
     for epoch in tqdm(range(num_epochs)):
         model.train()
         print("\nEpoch {}\n----------".format(epoch))
@@ -152,62 +156,223 @@ def TrainLoop(
             print("Loss for batch {} = {}".format(i, loss))
 
         print("\nTraining Loss for epoch {} = {}\n".format(epoch, train_loss))
-        total_train_loss.append(train_loss)
-
-        if val_dataloader is not None:
-            model.eval()
-            validation_loss = 0
-            with torch.inference_mode():
-                for batch, label in val_dataloader:
-                    batch, label = batch.to(device), label.to(device)
-                    outputs = model(batch)
-                    loss = criterion(outputs, label.float())
-                    validation_loss += loss
-
-                if validation_loss < best_val_loss:
-                    best_val_loss = validation_loss
-                    epochs_without_improvement = 0
-                else:
-                    epochs_without_improvement += 1
-
-                print(f"Current Validation Loss = {validation_loss}")
-                print(f"Best Validation Loss = {best_val_loss}")
-                print(f"Epochs without Improvement = {epochs_without_improvement}")
-
-            if epochs_without_improvement >= early_stopping_rounds:
-                print("Early Stoppping Triggered")
-                break
-            total_val_loss.append(validation_loss)
+        total_train_loss.append(train_loss/len(train_dataloader.dataset))
 
         model.eval()
-        test_loss = 0
+        validation_loss = 0
         with torch.inference_mode():
-            for batch, label in test_dataloader:
+            for batch, label in val_dataloader:
                 batch, label = batch.to(device), label.to(device)
                 outputs = model(batch)
                 loss = criterion(outputs, label.float())
-                test_loss += loss
+                validation_loss += loss
 
-            print("\nTest Loss for epoch {} = {}\n".format(epoch, test_loss))
-        total_test_loss.append(test_loss)
+            if validation_loss < best_val_loss:
+                best_val_loss = validation_loss
+                epochs_without_improvement = 0
+                best_model_weights = model.state_dict()
+            else:
+                epochs_without_improvement += 1
+
+            print(f"Current Validation Loss = {validation_loss}")
+            print(f"Best Validation Loss = {best_val_loss}")
+            print(f"Epochs without Improvement = {epochs_without_improvement}")
+
+        total_val_loss.append(validation_loss/len(val_dataloader.dataset))
+
+        if test_dataloader is not None:
+            test_loss = 0
+            with torch.inference_mode():
+                for batch, label in test_dataloader:
+                    batch, label = batch.to(device), label.to(device)
+                    outputs = model(batch)
+                    loss = criterion(outputs, label.float())
+                    test_loss += loss
+
+                print("\nTest Loss for epoch {} = {}\n".format(epoch, test_loss))
+            total_test_loss.append(test_loss/len(test_dataloader.dataset))
+
+        if epochs_without_improvement >= early_stopping_rounds:
+            print("Early Stoppping Triggered")
+            break
+
+    if return_best_model == True:
+        model.load_state_dict(best_model_weights)
 
     total_train_loss = [item.cpu().detach().numpy() for item in total_train_loss]
-    total_test_loss = [item.cpu().detach().numpy() for item in total_test_loss]
     total_val_loss = [item.cpu().detach().numpy() for item in total_val_loss]
 
     total_train_loss = np.array(total_train_loss)
-    total_test_loss = np.array(total_test_loss)
     total_val_loss = np.array(total_val_loss)
 
     x_train = np.arange(len(total_train_loss))
-    x_test = np.arange(len(total_test_loss))
     x_val = np.arange(len(total_val_loss))
+    sns.set_style('whitegrid')
+    try:
+        total_test_loss = [item.cpu().detach().numpy() for item in total_test_loss]
+        total_test_loss = np.array(total_test_loss)
+        x_test = np.arange(len(total_test_loss))
+        sns.lineplot(x=x_test, y=total_test_loss, label='Testing Loss')
+    except:
+        pass
 
     sns.lineplot(x=x_train, y=total_train_loss, label='Training Loss')
-    sns.lineplot(x=x_test, y=total_test_loss, label='Testing Loss')
     sns.lineplot(x=x_val, y=total_val_loss, label='Validation Loss')
-    plt.title("Loss after evey training epoch")
+    plt.title("Loss over {} Epochs".format(len(total_train_loss)))
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.xticks(np.arange(len(total_train_loss)))
+    plt.show()
+
+
+def TrainLoopv2(
+    model,
+    optimizer:torch.optim.Optimizer,
+    criterion:torch.nn.Module,
+    train_dataloader:torch.utils.data.DataLoader,
+    val_dataloader:torch.utils.data.DataLoader,
+    test_dataloader:torch.utils.data.DataLoader=None,
+    num_epochs:int=20,
+    early_stopping_rounds:int=5,
+    return_best_model:bool=True,
+    device:str='cpu'
+):
+    model.to(device)
+    best_val_loss = float('inf')
+    epochs_without_improvement = 0
+
+    total_train_loss = []
+    total_val_loss = []
+    total_test_loss = []
+    best_model_weights = model.state_dict()
+
+    train_accuracies = []
+    val_accuracies = []
+
+    for epoch in tqdm(range(num_epochs)):
+        model.train()
+        print("\nEpoch {}\n----------".format(epoch))
+        train_loss = 0
+        for i, (batch, label) in enumerate(train_dataloader):
+            batch, label = batch.to(device), label.to(device)
+            optimizer.zero_grad()
+            outputs = model(batch)
+            loss = criterion(outputs, label.float())
+            train_loss += loss
+            loss.backward()
+            optimizer.step()
+            print("Loss for batch {} = {}".format(i, loss))
+
+        print("\nTraining Loss for epoch {} = {}\n".format(epoch, train_loss))
+        total_train_loss.append(train_loss/len(train_dataloader.dataset))
+
+        model.eval()
+        validation_loss = 0
+        with torch.inference_mode():
+            val_true_labels = []
+            train_true_labels = []
+            val_pred_labels = []
+            train_pred_labels = []
+            for batch, label in val_dataloader:
+                batch, label = batch.to(device), label.to(device)
+                outputs = model(batch)
+                loss = criterion(outputs, label.float())
+                validation_loss += loss
+
+                outputs = torch.round(torch.sigmoid(outputs))
+                val_true_labels.extend(label.cpu().numpy())
+                val_pred_labels.extend(outputs.cpu().numpy())
+
+            for batch, label in train_dataloader:
+                batch, label = batch.to(device), label.to(device)
+                outputs = model(batch)
+
+                outputs = torch.round(torch.sigmoid(outputs))
+                train_true_labels.extend(label.cpu().numpy())
+                train_pred_labels.extend(outputs.cpu().numpy())
+
+            if validation_loss < best_val_loss:
+                best_val_loss = validation_loss
+                epochs_without_improvement = 0
+                best_model_weights = model.state_dict()
+            else:
+                epochs_without_improvement += 1
+
+            val_true_labels = np.array(val_true_labels)
+            train_true_labels = np.array(train_true_labels)
+            val_pred_labels = np.array(val_pred_labels)
+            train_pred_labels = np.array(train_pred_labels)
+
+            train_accuracy = accuracy_score(train_true_labels, train_pred_labels)
+            val_accuracy = accuracy_score(val_true_labels, val_pred_labels)
+
+            train_accuracies.append(train_accuracy)
+            val_accuracies.append(val_accuracy)
+
+            print(f"Current Validation Loss = {validation_loss}")
+            print(f"Best Validation Loss = {best_val_loss}")
+            print(f"Epochs without Improvement = {epochs_without_improvement}")
+
+            print(f"Train Accuracy: {train_accuracy * 100:.2f}%")
+            print(f"Validation Accuracy: {val_accuracy * 100:.2f}%")
+
+        total_val_loss.append(validation_loss/len(val_dataloader.dataset))
+
+        if test_dataloader is not None:
+            test_loss = 0
+            with torch.inference_mode():
+                for batch, label in test_dataloader:
+                    batch, label = batch.to(device), label.to(device)
+                    outputs = model(batch)
+                    loss = criterion(outputs, label.float())
+                    test_loss += loss
+
+                print("\nTest Loss for epoch {} = {}\n".format(epoch, test_loss))
+            total_test_loss.append(test_loss/len(test_dataloader.dataset))
+
+        if epochs_without_improvement >= early_stopping_rounds:
+            print("Early Stoppping Triggered")
+            break
+
+    if return_best_model == True:
+        model.load_state_dict(best_model_weights)
+    total_train_loss = [item.cpu().detach().numpy() for item in total_train_loss]
+    total_val_loss = [item.cpu().detach().numpy() for item in total_val_loss]
+
+    total_train_loss = np.array(total_train_loss)
+    total_val_loss = np.array(total_val_loss)
+
+    train_accuracies = np.array(train_accuracies)
+    val_accuracies = np.array(val_accuracies)
+
+    x_train = np.arange(len(total_train_loss))
+    x_val = np.arange(len(total_val_loss))
+    
+    sns.set_style('whitegrid')
+    plt.figure(figsize=(14,5))
+    
+    plt.subplot(1,2,1)
+    try:
+        total_test_loss = [item.cpu().detach().numpy() for item in total_test_loss]
+        total_test_loss = np.array(total_test_loss)
+        x_test = np.arange(len(total_test_loss))
+        sns.lineplot(x=x_test, y=total_test_loss, label='Testing Loss')
+    except:
+        pass
+
+    sns.lineplot(x=x_train, y=total_train_loss, label='Training Loss')
+    sns.lineplot(x=x_val, y=total_val_loss, label='Validation Loss')
+    plt.title("Loss over {} Epochs".format(len(total_train_loss)))
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.xticks(np.arange(len(total_train_loss)))
+    
+    plt.subplot(1,2,2)
+    sns.lineplot(x=x_train, y=train_accuracies, label='Training Accuracy')
+    sns.lineplot(x=x_val, y=val_accuracies, label='Validation Accuracy')
+    plt.title("Accuracy over {} Epochs".format(len(total_train_loss)))
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.xticks(np.arange(len(total_train_loss)))
+
     plt.show()
